a53911fecce297bfa65ccdcbcb29b7724ab1946f
Auzair-17 <auzair09270@gmail.com>
2026-02-08 14:49:10 +0430

    Home and ExcerciseDetail routes and our app basic structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExcerciseDetail from "./pages/ExcerciseDetail";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/excercise:id" element={<ExcerciseDetail />} />
+      </Routes>
+
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
